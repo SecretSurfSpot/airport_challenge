ef115fabda44d6fc630678b0b87f054ee6d56a84
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-11 13:54:04 +0100

    added argument to land_plane method

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,10 +3,10 @@ require_relative "plane"
 class Airport
 
   # Method creates new plane (airport will have a new plane when plane lands)
-  def land_plane
+  def land_plane(plane)
     Plane.new
   end
 
   def take_off_plane
-  end 
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,12 +4,15 @@ require "plane"
 describe Airport do
 
   it "Verifies land_plane exists" do
-    expect(subject).to respond_to :land_plane
+    expect(subject).to respond_to(:land_plane).with(1).argument
   end
+  # Can use single line code below:
+  # it { is_expected.to respond_to(:land_plane).with(1).argument }
+
 
   it "Verifies a plane landed" do
     # Need to verify a plane landed (landed = true?)
-    plane = subject.land_plane
+    plane = subject.land_plane(plane)
     expect(plane).to be_plane_exists
   end
 
